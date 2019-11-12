ffaf54c96b8f9b90b01dba7feee12ae3e5e8883f
nickzylstra <nick.zylstra@gmail.com>
2019-11-11 17:36:08 -0800

    Updated Update to Update Todo

--- datastore/index.js
+++ datastore/index.js
@@ -56,13 +56,26 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  const fileName = path.join(exports.dataDir, `${id}.txt`);
+  fs.open(fileName, 'r', (err, fd) => {
+    if (err) {
+      callback(err);
+      return new Error('Todo does not exist');
+    }
+
+    console.log(fd);
+    console.log(fileName);  
+    fs.writeFile(fileName, text, (err) => {
+      if (err) {
+        callback(err);
+        return new Error('Failed to update Todo file.');
+      }
+      callback(null, {
+        id,
+        text: text
+      });
+    });
+  });
 };
 
 exports.delete = (id, callback) => {
